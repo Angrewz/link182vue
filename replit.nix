95bb32b1ffb62714aa1561d7a8043416169b39f7
Angrewz <97853812+Angrewz@users.noreply.github.com>
2023-11-01 23:54:28 +0300

    Add files via upload

---
+++ replit.nix
@@ -0,0 +1,11 @@
+{ pkgs }: {
+    deps = [
+      pkgs.screen
+        pkgs.yarn
+        pkgs.esbuild
+        pkgs.nodejs-18_x
+
+        pkgs.nodePackages.typescript
+        pkgs.nodePackages.typescript-language-server
+    ];
+}
